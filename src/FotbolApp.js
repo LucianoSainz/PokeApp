46c4554f321a58b12d30c944034dc30f77d29e98
LucianoSainz <danielsainz28@gmail.com>
2021-06-29 11:54:19 +0200

    added botton add player

--- src/FotbolApp.js
+++ src/FotbolApp.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 
 
 const FotbolApp = () => {
+
+ 
+   const[players, setPlayers] = useState(['Messi', 'Ronaldo', 'Suarez']);
+   
+   const handlerAdd = () => {
+
+       //setPlayers (['Kroos', ...players]);
+
+       setPlayers( play => ['kroos', ...play]);
+   }
+   
+
    return(
        <>
        <h2>FotbolApp</h2>
        <hr />
+
+       <button onClick = {handlerAdd}>Add</button>
+
+       <ol>
+           {
+               players.map(player => {
+                   return <li key={player}>{player}</li>
+               })
+           }
+       </ol>
        </>
    ) 
 }
