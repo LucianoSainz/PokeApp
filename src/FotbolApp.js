9aaf8067adbb727c785058c775c62feddb0219e9
LucianoSainz <danielsainz28@gmail.com>
2021-06-29 11:28:07 +0200

    starting to give structure to the app

---
+++ src/FotbolApp.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+
+const FotbolApp = () => {
+   return(
+       <>
+       <h2>FotbolApp</h2>
+       <hr />
+       </>
+   ) 
+}
+
+export default FotbolApp;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import FotbolApp from './FotbolApp';
+import './index.css';
+
+
+ReactDOM.render(
+    <FotbolApp />,
+  document.getElementById('root')
+);
+
+
