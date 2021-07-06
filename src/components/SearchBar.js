1335b5cb789bc9459f2b573f64b2692852140f22
LucianoSainz <danielsainz28@gmail.com>
2021-07-06 09:03:21 +0200

    added fetch with api url

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -9,13 +9,19 @@ const handleChange = (e) =>{
     setSearch(e.target.value);
 }
 
+const handleClick = (e) => {
+   console.log('Apretando boton');
+}
+
     return (
         <div>
         <div>
             <input type='text' placeholder='buscar pokemon'
              onChange={handleChange} />
         </div>
-        <div>{search}</div>
+        <div>
+            <button onClick={handleClick}>Buscar</button>
+        </div>
         </div>
     )
 }
