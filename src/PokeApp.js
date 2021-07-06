e1a005bbe8d5a0598b4a534daacf960bcc540500
LucianoSainz <danielsainz28@gmail.com>
2021-07-06 11:54:22 +0200

    requesting information from the api

--- src/PokeApp.js
+++ src/PokeApp.js
@@ -1,15 +1,35 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Navbar  from './components/Navbar';
+import PokeRes from './components/PokeRes';
 import SearchBar from './components/SearchBar';
+import { getPokemons } from './api';
 
 
 
  export default function PokeApp() {
+
+    const [pokemons, setPokemons] = useState([]);
+
+    const fetchgetPokemons = async () => {
+        try{
+            const data = await getPokemons();
+             console.log(data);
+        } catch (err){
+
+        }
+
+    }
+
+    useEffect(() => {
+       fetchgetPokemons();
+    }, [])
+
    return(
        <div>
            <Navbar />
        <div className='PokeApp'>
            <SearchBar />
+           <PokeRes />
        </div>
        </div>
    ) 
