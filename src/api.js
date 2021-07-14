723d75d38f460ac4196705bc4742c65d56a8066d
LucianoSainz <danielsainz28@gmail.com>
2021-07-14 20:12:38 +0200

    added dependencies

--- src/api.js
+++ src/api.js
@@ -29,7 +29,7 @@ export const getPokemonData = async (url) => {
         const response = await fetch(url);
         const data = await response.json()
         return data;
-    } catch {
+    } catch (err){
 
     }
 }
